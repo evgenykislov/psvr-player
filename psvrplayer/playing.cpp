531624750d026bb9dfe591a36c62c34e6d735edf
Evgeny Kislov <dev@evgenykislov.com>
2024-10-19 17:58:53 +0300

    Fixes fastforward. Task https://github.com/users/evgenykislov/projects/1/views/1?pane=issue&itemId=83224213.

--- psvrplayer/playing.cpp
+++ psvrplayer/playing.cpp
@@ -24,13 +24,13 @@ int scancode_left_ctrl_ = 37;
 bool pause_state_ = false;
 
 struct KeySeq {
-  std::chrono::steady_clock::time_point
-      pre_press;  //!< Время предыдущего нажатия
-  std::chrono::steady_clock::time_point
-      pre_pre_press;  //!< Время пред-предыдущего нажатия
+  //! Время предыдущего нажатия
+  std::chrono::steady_clock::time_point PrePress;
+  //! Признак, что предыдущее нажатие было "быстрым" (в мультицепочке)
+  bool PreMultiFlag;
 };
 
-const auto kMultiKeyInterval = std::chrono::milliseconds(1000);
+const auto kMultiKeyInterval = std::chrono::milliseconds(500);
 
 enum MultiKey { kSingleKey, kDoubleKey, kTripleKey };
 
@@ -45,36 +45,39 @@ double central_y;
 double last_x;
 double last_y;
 
+void KeyClear(KeySeq& seq) {
+  seq.PrePress = std::chrono::steady_clock::time_point();
+  seq.PreMultiFlag = false;
+}
 
 void KeyAction(KeySeq& seq, MultiKey& multi) {
   auto cur = std::chrono::steady_clock::now();
-  auto cur_mi = cur - kMultiKeyInterval;
-  if (seq.pre_pre_press >= cur_mi && seq.pre_pre_press < cur) {
-    multi = kTripleKey;
-  } else if (seq.pre_press >= cur_mi && seq.pre_press < cur) {
-    multi = kDoubleKey;
-  } else {
-    multi = kSingleKey;
+  if ((cur - kMultiKeyInterval) < seq.PrePress) {
+    // Мультинажатие
+    multi = seq.PreMultiFlag ? kTripleKey : kDoubleKey;
+    seq.PreMultiFlag = true;
+    seq.PrePress = cur;
+    return;
   }
 
-  seq.pre_pre_press = seq.pre_press;
-  seq.pre_press = cur;
+  // Одиночное нажатие
+  multi = kSingleKey;
+  seq.PreMultiFlag = false;
+  seq.PrePress = cur;
 }
 
 int GetMovement(MultiKey m) {
   if (m == kTripleKey) {
-    return 300;
+    return 300 - 30;
   }
   if (m == kDoubleKey) {
-    return 30;
+    return 30 - 5;
   }
   return 5;
 }
 
 void KeyProcessor(int key, int scancode, int action, int mods,
     std::shared_ptr<IVideoPlayer> player, std::shared_ptr<IHelmet> helmet) {
-  std::cout << "Key: " << key << ", scan " << scancode << ", action " << action
-            << ", mods " << mods << std::endl;
   if (scancode == scancode_space_ && action == GLFW_PRESS && mods == 0) {
     pause_state_ = !pause_state_;
     player->Pause(pause_state_);
@@ -87,6 +90,7 @@ void KeyProcessor(int key, int scancode, int action, int mods,
   }
 
   if (scancode == scancode_left_arrow_ && action == GLFW_PRESS && mods == 0) {
+    KeyClear(right_arrow_seq_);
     MultiKey m;
     KeyAction(left_arrow_seq_, m);
     player->Move(-GetMovement(m));
@@ -94,6 +98,7 @@ void KeyProcessor(int key, int scancode, int action, int mods,
   }
 
   if (scancode == scancode_right_arrow_ && action == GLFW_PRESS && mods == 0) {
+    KeyClear(left_arrow_seq_);
     MultiKey m;
     KeyAction(right_arrow_seq_, m);
     player->Move(GetMovement(m));
