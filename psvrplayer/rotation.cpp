466279cc6562f28c27ef1a958e287b5de7f60133
Evgeny Kislov <dev@evgenykislov.com>
2024-12-21 21:25:49 +0300

    Improve algorithm of angles calculation.

--- psvrplayer/rotation.cpp
+++ psvrplayer/rotation.cpp
@@ -2,8 +2,8 @@
 
 
 const double kPi = 3.1415926535897932384626433832795;
-const double kNearZeroLength2 =
-    1.0e-8;  //!< Длина очень короткого вектора. При расчётах углов означает
+const double kNearZeroLength =
+    1.0e-4;  //!< Длина очень короткого вектора. При расчётах углов означает
              //!< взгляд ровно вверх или вниз
 
 
@@ -31,26 +31,35 @@ void Rotation::Rotate(double right1, double top1, double clock1) {
 
 
 void Rotation::GetSummRotation(glm::mat4& rot_mat) {
-  double right_angle = atan2(view_.x, view_.z);
-  double top_angle;
-  if (view_.y >= 1.0) {
-    top_angle = kPi / 2.0;
-  } else if (view_.y <= -1.0) {
-    top_angle = -kPi / 2.0;
+  double clock_angle = 0.0;
+  double right_angle = 0.0;
+  double top_angle = 0.0;
+
+  // Отработаем случай, когда взгляд идёт ровно вверх или ровно вниз
+  vec3d zenith(0.0, 1.0, 0.0);
+  auto top_dev = glm::cross(view_, vec3d(0.0, 1.0, 0.0));
+  if (glm::length(top_dev) < kNearZeroLength) {
+    // Взгляд ровно верх или вниз
+    clock_angle = 0.0;  // Считаем, что шлем не наклонён
+    if (view_.y > 0.0) {
+      // Смотрим ровно вверх
+      // Горизонтальный угол считаем как противоположный для вершины
+      right_angle = atan2(-tip_.x, -tip_.z);
+      top_angle = kPi / 2.0;
+    } else {
+      // Смотрим ровно вниз
+      right_angle = atan2(tip_.x, tip_.z);
+      top_angle = -kPi / 2.0;
+    }
   } else {
+    right_angle = atan2(view_.x, view_.z);
     top_angle = asin(view_.y);
+
+    clock_angle = RadAngle(view_, tip_, zenith);
   }
 
-  auto horz = glm::cross(view_, vec3d(0.0, 1.0, 0.0));
-  double clock_angle;
-  if (glm::length2(horz) < kNearZeroLength2) {
-    clock_angle = 0.0;
-  } else {
-    clock_angle = kPi / 2.0 - glm::angle(glm::normalize(horz), tip_);
-  }
-
-  // Последовательность поворотов: кручение (roll_angle), подъём (top_angle),
-  // в горизонтальной плоскости (right_angle)
+  // Последовательность (обратная) поворотов: кручение (roll_angle), подъём
+  // (top_angle), в горизонтальной плоскости (right_angle)
   glm::mat4 r1 = glm::rotate(
       glm::mat4(1.0f), float(clock_angle), glm::vec3(0.0f, 0.0f, 1.0f));
   glm::mat4 r2 =
@@ -58,4 +67,28 @@ void Rotation::GetSummRotation(glm::mat4& rot_mat) {
   glm::mat4 r3 =
       glm::rotate(r2, float(right_angle), glm::vec3(0.0f, 1.0f, 0.0f));
   rot_mat = r3;
+}
+
+
+double Rotation::RadAngle(const vec3d& n, const vec3d& v1, const vec3d& v2) {
+  auto p1 = glm::cross(n, v1);
+  auto p2 = glm::cross(n, v2);
+  auto raw_d = glm::dot(p1, p2);
+  auto cos2 = (raw_d * raw_d) / glm::length2(p1) / glm::length2(p2);
+
+  double angle = 0.0;
+  if (cos2 >= 1.0) {
+    angle = 0.0;
+  } else {
+    angle = glm::acos(2.0 * cos2 - 1.0) / 2.0;
+    if (raw_d < 0) {
+      angle = kPi - angle;
+    }
+  }
+
+  if (glm::dot(glm::cross(p1, p2), n) < 0) {
+    return -angle;
+  }
+
+  return angle;
 }
--- unittests/rotation_view.cpp
+++ unittests/rotation_view.cpp
@@ -49,31 +49,44 @@ void GenerateRotation(track& newtrack) {
 }
 */
 
-void GenerateLeftRight(track& newtrack) {
-  newtrack.clear();
-  const float dangle = 0.1;
 
-  for (float angle = -90; angle < 90; angle += dangle) {
-    Point p;
-    p.Helmet.x = cos(glm::radians(angle));
-    p.Helmet.y = 0.0;
-    p.Helmet.z = sin(glm::radians(angle));
-    p.Tip = glm::vec3(0.0, 1.0, 0.0);
-    newtrack.push_back(p);
+/*! Сосчитаем угол поворота между векторами в заданной плоскости. Поворот
+считается против часовой стрелки с точки зрения вектора нормали. Система
+координат: х - вправо, y - вверх, z - вперёд от нас \param n плоскость, в
+которой считаются углы. Задаётся нормалью \param v1 вектор от которого считается
+поворот \param v2 вектор в сторону которого считается поворот \return угол
+поворота в градусах (от -180 до +180) */
+double Angle(const vec3d& n, const vec3d& v1, const vec3d& v2) {
+  auto p1 = glm::cross(n, v1);
+  auto p2 = glm::cross(n, v2);
+  auto raw_d = glm::dot(p1, p2);
+  auto cos2 = (raw_d * raw_d) / glm::length2(p1) / glm::length2(p2);
+
+  double angle = 0.0;
+  if (cos2 >= 1.0) {
+    angle = 0.0;
+  } else {
+    angle = glm::degrees(glm::acos(2.0 * cos2 - 1.0)) / 2.0;
+    if (raw_d < 0) {
+      angle = 180.0 - angle;
+    }
   }
+
+  if (glm::dot(glm::cross(p1, p2), n) < 0) {
+    return -angle;
+  }
+
+  return angle;
 }
 
 
 /*! Подсчёт угловых перемещений от точки p1 к точке p2 */
 void GetAngles(Point p1, Point p2, double& right_angle, double& top_angle,
     double& clock_angle) {
-  // right angle
-  auto n1 = glm::normalize(glm::cross(p1.Helmet, p1.Tip));
-  auto n2 = glm::normalize(glm::cross(p2.Helmet, p1.Tip));
-  right_angle = glm::degrees(glm::acos(glm::dot(n1, n2)));
-
-  top_angle = 0.0f;
-  clock_angle = 0.0f;
+  right_angle = Angle(p1.Tip, p1.Helmet, p2.Helmet);
+  auto top_axis = glm::normalize(glm::cross(p1.Tip, p1.Helmet));
+  top_angle = Angle(top_axis, p1.Helmet, p2.Helmet);
+  clock_angle = Angle(p1.Helmet, p1.Tip, p2.Tip);
 }
 
 double Distance(glm::vec3 a, glm::vec3 b) {
@@ -96,21 +109,50 @@ void CheckTrack(const track& t) {
     glm::mat4 m;
     rt.GetSummRotation(m);
 
-    auto view = m * glm::vec4(it->Helmet, 1.0);
-    auto tip = m * glm::vec4(it->Tip, 1.0);
+    auto view = m * glm::vec4(base.Helmet, 1.0);
+    auto tip = m * glm::vec4(base.Tip, 1.0);
 
-    //    std::cout << "Distance: " << Distance(view, base.Helmet) << ", " <<
-    //    Distance(tip, base.Tip) << std::endl;
+    //    std::cout << "Distance: " << Distance(view, it->Helmet) << ", " <<
+    //    Distance(tip, it->Tip) << std::endl;
 
-    EXPECT_LE(Distance(view, base.Helmet), kSuitableNearZeroLength);
-    EXPECT_LE(Distance(tip, base.Tip), kSuitableNearZeroLength);
+    EXPECT_LE(Distance(view, it->Helmet), kSuitableNearZeroLength);
+    EXPECT_LE(Distance(tip, it->Tip), kSuitableNearZeroLength);
   }
 }
 
 
 TEST(LeftRightView, Mathematics) {
   track t;
-  GenerateLeftRight(t);
+  t.clear();
+  const float dangle = 0.1;
+
+  for (float angle = -90; angle < 90; angle += dangle) {
+    Point p;
+    p.Helmet.x = cos(glm::radians(angle));
+    p.Helmet.y = 0.0;
+    p.Helmet.z = sin(glm::radians(angle));
+    p.Tip = glm::vec3(0.0, 1.0, 0.0);
+    t.push_back(p);
+  }
+  CheckTrack(t);
+}
+
+
+TEST(TopDownView, Mathematics) {
+  track t;
+  t.clear();
+  const float dangle = 90;
+
+  for (float angle = -80; angle < 80; angle += dangle) {
+    Point p;
+    p.Helmet.x = 0.0;
+    p.Helmet.y = sin(glm::radians(angle));
+    p.Helmet.z = cos(glm::radians(angle));
+    p.Tip.x = 0.0;
+    p.Tip.y = cos(glm::radians(angle));
+    p.Tip.z = -sin(glm::radians(angle));
+    t.push_back(p);
+  }
   CheckTrack(t);
 }
 
